57f137bb4fd8b3e4ce1d3f66cd27bfcfcf057733
Marius Conjeaud <marius.conjeaud@neo4j.com>
2025-03-14 16:16:18 +0100

    Add memgraph to integration tests

--- test/async_/conftest.py
+++ test/async_/conftest.py
@@ -24,6 +24,8 @@ async def setup_neo4j_session(request):
         "NEO4J_BOLT_URL", "bolt://neo4j:foobarbaz@localhost:7687"
     )
 
+    config.DATABASE_FLAVOUR = os.environ.get("DATABASE_FLAVOUR", DatabaseFlavour.NEO4J)
+
     # Clear the database if required
     database_is_populated, _ = await adb.cypher_query(
         "MATCH (a) return count(a)>0 as database_is_populated"
--- test/sync_/conftest.py
+++ test/sync_/conftest.py
@@ -24,6 +24,8 @@ def setup_neo4j_session(request):
         "NEO4J_BOLT_URL", "bolt://neo4j:foobarbaz@localhost:7687"
     )
 
+    config.DATABASE_FLAVOUR = os.environ.get("DATABASE_FLAVOUR", DatabaseFlavour.NEO4J)
+
     # Clear the database if required
     database_is_populated, _ = db.cypher_query(
         "MATCH (a) return count(a)>0 as database_is_populated"
